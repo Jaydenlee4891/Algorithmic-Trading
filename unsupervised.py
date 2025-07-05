62bf62592797359779970bb305a18e52d98bc205
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-07-05 22:49:41 +0900

    Update unsupervised.py

--- unsupervised.py
+++ unsupervised.py
@@ -11,6 +11,8 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_s%26P_500_companies')[0]
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 
-sp500['symbol'] = sp500['Symbol'].str
+sp500['Symbol'] = sp500['Symbol'].str.replace('.','-')
+
+symbols_list = sp500['Symbol'].unique().tolist()
