e7ff2ee46ccd6587e07f7e5e4b800c2fe8004c36
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-07-06 13:57:57 +0900

    testing
    tests

--- unsupervised.py
+++ unsupervised.py
@@ -16,3 +16,5 @@ sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 sp500['Symbol'] = sp500['Symbol'].str.replace('.','-')
 
 symbols_list = sp500['Symbol'].unique().tolist()
+
+
